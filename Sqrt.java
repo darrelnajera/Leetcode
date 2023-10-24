70eed2d6d95301124d3b901c7568ccce31f955bf
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-10-23 23:50:54 -0500

    Update Sqrt.java

--- Sqrt.java
+++ Sqrt.java
@@ -1,12 +1,18 @@
 public class Sqrt {
     class Solution {
         public int mySqrt(int x) {
+            //store variable for solution 
             int sqrt=0;
             int i=1;
+            //while input is more than 0
             while(x>0){
+                //subtract 1 from input until it reaches 0
                 x-=i;
+                //if input is less than 0 stop subtracting
                 if(x<0) break;
+                //add 2 to input to reach sqrt
                 i+=2;
+                //add solution to square root variable we stored above
                 sqrt++;
             }
             return sqrt;
